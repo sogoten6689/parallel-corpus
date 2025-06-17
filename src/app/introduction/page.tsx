5ada421bcd510b88fa81a72c6bdfaae72a692a69
Lam Nguyen <24C11012@student.hcmus.edu.vn>
2025-06-18 00:42:43 +0700

    update ui and menu

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -3,6 +3,7 @@
 import InDevelop from "@/components/ui/in-develop";
 import { Typography } from "antd";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
 
 const Introduction: React.FC = () => {
   const { t } = useTranslation();
@@ -11,6 +12,48 @@ const Introduction: React.FC = () => {
     <div className="grid items-center justify-items-center">
        <Typography.Title level={5} style={{ margin: 0 }} title="Introduction">{t("introduction")}</Typography.Title> 
       <InDevelop />
+
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Introduction
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          About us
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Contact us
+        </a>
+      </footer>
     </div>
   );
 }
