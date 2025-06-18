eb6f0579323a50671261317578f3b6916cbe4946
Lam Nguyen <24C11012@student.hcmus.edu.vn>
2025-06-18 18:52:21 +0700

    update route and update translate

--- src/components/ui/corpus-table.tsx
+++ src/components/ui/corpus-table.tsx
@@ -1,27 +1,58 @@
 'use client';
 
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Tooltip } from 'antd';
 import { RowWord } from '@/types/row-word.type';
-
-const columns = [
-  { title: 'ID', dataIndex: 'ID', key: 'ID' },
-  { title: 'Word', dataIndex: 'Word', key: 'Word' },
-  { title: 'Lemma', dataIndex: 'Lemma', key: 'Lemma' },
-  { title: 'Links', dataIndex: 'Links', key: 'Links' },
-  { title: 'Morph', dataIndex: 'Morph', key: 'Morph' },
-  { title: 'POS', dataIndex: 'POS', key: 'POS' },
-  { title: 'Phrase', dataIndex: 'Phrase', key: 'Phrase' },
-  { title: 'Grm', dataIndex: 'Grm', key: 'Grm' },
-  { title: 'NER', dataIndex: 'NER', key: 'NER' },
-  { title: 'Semantic', dataIndex: 'Semantic', key: 'Semantic' },
-];
+import { useTranslation } from 'react-i18next';
 
 type CorpusTableProps = {
   data: RowWord[]
 }
 
+const getColumnKey = (key: string) => {
+  switch (key) {
+    case 'id':
+      return 'ID';
+    case 'word':
+      return 'Word';
+    case 'Lemma':
+      return 'Lemma';
+    case 'links':
+      return 'Links';
+    case 'morph':
+      return 'Morph';
+    case 'pos':
+      return 'POS';
+    case 'phrase':
+      return 'Phrase';
+    case 'grm':
+      return 'Grm';
+    case 'ner':
+      return 'NER';
+    case 'semantic':
+      return 'Semantic';
+    default:
+      return key;
+  }
+}
+
 export default function CorpusTable({ data }: CorpusTableProps) {
+const { t } = useTranslation();
+
+
+// Giả sử bạn dùng i18n
+const getColumnWithTooltip = (key: string) => ({
+  title: <Tooltip title={t(`${key}_tooltip`)}>{t(key)}</Tooltip>,
+  dataIndex: getColumnKey(key),
+  key: getColumnKey(key),
+});
+
+// Danh sách các cột
+const columnKeys = ['id', 'word', 'lemma', 'links', 'morph', 'pos', 'phrase', 'grm', 'ner', 'semantic'];
+
+// Tạo columns
+const columns = columnKeys.map(getColumnWithTooltip);
+
   return (
     <div style={{ padding: 16 }}>
       <Table
